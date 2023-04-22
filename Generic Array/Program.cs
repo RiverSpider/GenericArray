49f1be0b0df2c43dd621843bc8ba1dd2d5abd087
RiverSpider <raevskih8888@mail.ru>
2023-04-22 18:22:34 +0600

    Add project files.

---
+++ Generic Array/Program.cs
@@ -0,0 +1,40 @@
+﻿public class GenericArray<T>
+{
+    private T[] array;
+    private int size;
+
+    public GenericArray(int capacity)
+    {
+        this.array = new T[capacity];
+        this.size = 0;
+    }
+
+    public void Add(T element)
+    {
+        if (size == array.Length)
+        {
+            Array.Resize(ref array, array.Length * 2);
+        }
+        array[size++] = element;
+    }
+
+    public T Get(int index)
+    {
+        if (index < 0 || index >= size)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        return array[index];
+    }
+
+    public void Remove(int index)
+    {
+        if (index < 0 || index >= size)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        Array.Copy(array, index + 1, array, index, size - index - 1);
+        array[--size] = default(T);
+    }
+}
+
